641ce1b8a15d8d73151d92a027157736d6263fc5
Frederik Lassen <frederiklassen@gmail.com>
2015-11-18 18:57:53 +0100

    Adds basic functionality to entry point

--- src/sudizzle.fsx
+++ src/sudizzle.fsx
@@ -1,2 +1,95 @@
-(* Main entry point for application *
-)
+(* Main entry point for application *)
+[<EntryPoint>]
+let main args =
+  printfn "%s" """
+  Velkommen til SuDizzle, du har nu følgende valgmuligheder:
+
+  - 'start'   for at starte et nyt spil
+  - 'indlæs'  for at indlæse et tidligere spil
+  """
+
+  // Keeps asking the user for input with recursion
+  let rec interrogate() = 
+    printf "%s" "> " 
+    let input = System.Console.ReadLine()
+    match input with
+      | "start"   -> help(); newGame()
+      | "indlæs"  -> help(); loadGame()
+      | _ -> 
+        printf "%s" "Forkert input, prøv igen!\n";
+        interrogate()
+
+  interrogate()
+
+  // Return 0 to indicate success
+  0
+
+let help() =
+  printfn "%s" """
+  Spillet understøtter følgende kommandoer:
+
+  help  - bringer denne hjælp frem
+  save  - gemmer spillet
+
+  Desuden spilles der ved fx. at skrive:
+
+  (3,2)=2
+  322
+  3,2,2
+
+  Hvor de tre tal repræsenterer, i denne rækkefølge:
+
+  - række
+  - søjle
+  - værdi
+
+  God spillelyst!
+  """
+
+let newGame() =
+  loop(Sudoku.generate())
+
+let loadGame() =
+  printfn "%s" """
+  Indtast venligst navnet på det gemte spil
+  """
+
+  let rec interrogate() = 
+    printf "%s" "> " 
+    let input = System.Console.ReadLine()
+    match input with
+    | "save" -> 
+
+
+  loop(sudoku)
+
+let mutable savedAs = ""
+
+let saveGame() =
+  
+
+let stripNonNumbers input =
+  String.map (fun c -> if System.Char.IsNumber(c) then c else char(0)) input
+
+let rec loop sudoku =
+  // Print the sudoku to screen
+  Sudoku.print sudoku
+
+  // Keep asking the user for input with recursion
+  let rec interrogate() =
+    printf "%s" "> "
+    let input = System.Console.ReadLine()
+    match input with
+    | "save" -> saveGame()
+    | "help" -> help()
+    | _ ->
+      let numbers = stripNonNumbers input
+      if String.length numbers = 3 then
+        let r = 48 - int(numbers[0])
+        let s = 48 - int(numbers[1])
+        let v = 48 - int(numbers[2])
+
+        loop (Sudoku.update sudoku (r,s,v))
+      else
+        interrogate()
+
