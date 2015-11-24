f26748e6615000ed64c911920c6c20310450bdd9
Frederik Lassen <frederiklassen@gmail.com>
2015-11-24 15:30:57 +0100

    Small fixes and isFinished

--- src/lib/sudoku.fs
+++ src/lib/sudoku.fs
@@ -12,13 +12,17 @@ let region (r, s) (list : board) =
     let r, s = (r / 3 * 3, s / 3 * 3)
     List.collect (fun (x : row) -> x.[s .. s + 2]) list.[r .. r + 2]
 
+let missin' = Set.difference (set span)
+
 let hints (r, s) (list : board) =
-    let missin' = Set.difference (set span)
     seq [set list.[r] |> missin'                                   // Horizontal
          set (transpose list).[s] |> missin'                       // Vertical
          set (region (r, s) list) |> missin'] |> Set.intersectMany // Region
 
-let rec insert (r, s) v list =
+let isFinished list =
+    List.forall (fun x -> (missin' x).Length = 0) list
+
+let insert (r, s) v list =
     if not ((hints (r, s) list).Contains v) then None else
     List.mapi (fun i x -> if i <> r then x else
                           List.mapi (fun i x -> if i <> s then x else v) x) list |> Some
