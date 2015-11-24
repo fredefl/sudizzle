84f4710656e2653d70b5b0b90c9b4297fe8a9933
Emil Unn Weihe <e@weihe.dk>
2015-11-24 20:01:20 +0100

    Documentizzle

--- src/lib/sudoku.fs
+++ src/lib/sudoku.fs
@@ -1,8 +1,10 @@
 module Sudoku
 
-let span = [1 .. 9]
 type row = list<int>
 type board = list<row>
+
+let span = [1 .. 9]
+let missin' = Set.difference (set span)
 
 let rec transpose = function
     | (_ :: _) :: _ as list -> List.map List.head list :: transpose (List.map List.tail list)
@@ -12,15 +14,13 @@ let region (r, s) (list : board) =
     let r, s = (r / 3 * 3, s / 3 * 3)
     List.collect (fun (x : row) -> x.[s .. s + 2]) list.[r .. r + 2]
 
-let missin' = Set.difference (set span)
-
 let hints (r, s) (list : board) =
-    seq [set list.[r] |> missin'                                   // Horizontal
-         set (transpose list).[s] |> missin'                       // Vertical
-         set (region (r, s) list) |> missin'] |> Set.intersectMany // Region
+    seq [set list.[r] |> missin'
+         set (transpose list).[s] |> missin'
+         set (region (r, s) list) |> missin'] |> Set.intersectMany
 
 let isFinished (list : board) = 
-    List.forall (fun (x : row) -> (missin' (set x)).Count = 0) list
+    List.forall (fun x -> (missin' (set x)).Count = 0) list
 
 let insert (r, s) v list =
     if not ((hints (r, s) list).Contains v) then None else
@@ -38,6 +38,8 @@ let parse input =
 let stringify list =
     List.map (fun x -> List.fold (fun acc y -> acc + if y = 0 then "*" else y.ToString ()) "" x) list
 
-let load file = System.IO.File.ReadAllLines file |> List.ofArray |> parse
+let load file =
+    System.IO.File.ReadAllLines file |> List.ofArray |> parse
 
-let save file list = System.IO.File.WriteAllLines (file, stringify list)    
+let save file list =
+    System.IO.File.WriteAllLines (file, stringify list)    
--- src/lib/sudoku.fsi
+++ src/lib/sudoku.fsi
@@ -3,18 +3,34 @@ module Sudoku
 type row = list<int>
 type board = list<row>
 
-/// Finder de tal som kan indsættes på en given plads.
+/// <summary><c>hints</c>-funktionen finder de værdier som kan indsættes i et givent felt.</summary>
+/// <param name = "felt"></param>
+/// <param name = "bræt"></param>
+/// <returns><c>set</c> af værdier.</returns>
 val hints : (int * int) -> board -> Set<int>
 
-/// Tjekker om alle rækker indeholder alle tal fra 1 - 9.
+/// <summary><c>isFinished</c> tjekker om alle rækker indeholder tal fra 1 - 9.</summary>
+/// <param name = "bræt"></param>
+/// <returns>Hvorvidt om spillet er ovre.</returns>
 val isFinished : board -> bool
 
-/// Indsætter et element. 
+/// <summary>Indsætter en værdi.</summary>
+/// <param name = "felt"></param>
+/// <param name = "værdi"></param>
+/// <param name = "bræt"></param>
+/// <returns>Shizz'</returns>
 val insert : (int * int) -> int -> board -> board option
 
-/// Printer Soduko plade.
+/// <summary>Printer Soduko plade.</summary>
+/// <param name = "bræt"></param>
 val print : board -> unit
 
+/// <summary>Loader et bræt fra fil.</summmary>
+/// <param name = "filnavn"></param>
+/// <returns>Et bræt</returns>
 val load : string -> board
 
+/// <summary>Gemmer et bræt til fil.</summmary>
+/// <param name = "filnavn"></param>
+/// <param name = "bræt"></param>
 val save : string -> board -> unit
